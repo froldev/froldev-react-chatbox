a5f0e28ca4eb7f4e884ae86903257e617f206d08
Froldev <frol.developpement@gmail.com>
2020-09-05 16:56:33 +0200

    connexion with react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React, { Component } from 'react'
-import './App.css'
+import React, { Component } from 'react';
+import './App.css';
 
 class App extends Component {
-  render () {
-    return (
-      <div className='box' />
-    )
-  }
+	render() {
+		return (
+			<div className='box'>
+				<h1>Accueil</h1>
+			</div>
+		);
+	}
 }
 
-export default App
+export default App;
--- src/components/Connexion.js
+++ src/components/Connexion.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 
 class Connexion extends Component {
+	state = {
+		pseudo: '',
+		goToChat: false,
+	};
+
+	handleChange = event => {
+		const pseudo = event.target.value;
+		this.setState({ pseudo });
+	};
+
+	handleSubmit = event => {
+		event.preventDefault();
+		this.setState({ goToChat: true });
+	};
+
 	render() {
+		if (this.state.goToChat) {
+			return <Redirect push to={`/pseudo/${this.state.pseudo}`}></Redirect>;
+		}
+
 		return (
 			<div className='connexionBox'>
-				<form className='connexion'>
+				<form className='connexion' onSubmit={this.handleSubmit}>
 					<input
+						value={this.state.pseudo}
+						onChange={this.handleChange}
 						placeholder='Pseudo'
 						type='text'
 						required
