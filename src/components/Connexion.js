06563f0cfd73806fbed7939fcf657b756884af3f
Froldev <frol.developpement@gmail.com>
2020-09-05 16:04:58 +0200

    connexion page

---
+++ src/components/Connexion.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Connexion extends Component {
+	render() {
+		return (
+			<div className='connexionBox'>
+				<form className='connexion'>
+					<input
+						placeholder='Pseudo'
+						type='text'
+						required
+					/>
+					<button type='submit'>Go</button>
+				</form>
+			</div>
+		);
+	}
+}
+
+export default Connexion;
