7ae333d625f6ae789affd1550927f149596935fe
Froldev <frol.developpement@gmail.com>
2020-09-05 17:14:38 +0200

    save messages in the state

--- src/components/Formulaire.js
+++ src/components/Formulaire.js
@@ -1,10 +1,47 @@
 import React, { Component } from 'react';
 
 class Formulaire extends Component {
+	state = {
+		message: ''
+	};
+
+	createMessage = () => {
+		const { addMessage, pseudo } = this.props;
+
+		const message = {
+			pseudo: pseudo,
+			message: this.state.message,
+		};
+
+		addMessage(message);
+
+		// reset
+		this.setState({ message: '' });
+	};
+
+	handleSubmit = event => {
+		event.preventDefault();
+		this.createMessage();
+	};
+
+	handleChange = event => {
+		const message = event.target.value;
+		this.setState({ message });
+	};
+
+	handleKeyUp = event => {
+		if (event.key === 'Enter') {
+			this.createMessage();
+		}
+	};
+
 	render() {
 		return (
-			<form className='form'>
+			<form className='form' onSubmit={this.handleSubmit}>
 				<textarea
+					value={this.state.message}
+					onChange={this.handleChange}
+					onKeyUp={this.handleKeyUp}
 					maxLength='140'
 					required
 				/>
